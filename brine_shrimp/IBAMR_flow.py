d56b164eeef8a3a6a53076c0675356814b7bdae2
Christopher Strickland <mountaindust@gmail.com>
2019-04-10 11:24:20 -0400

    IBAMR_flow.py now looks like PIV experiment

--- brine_shrimp/IBAMR_flow.py
+++ brine_shrimp/IBAMR_flow.py
@@ -1,4 +1,8 @@
 #! /usr/bin/env python3
+'''
+Read IBFE data for 3D cylinder model. Reproduce flowtank experiment with
+random walk agents.
+'''
 
 import sys
 sys.path.append('..')
@@ -10,27 +14,43 @@ import numpy as np
 import Planktos, data_IO
 
 # Whether or not to show the cylinders based on the mesh data
-PLOT_CYL = True
+PLOT_CYL = False
 
 # Intialize environment
 envir = Planktos.environment()
 
 # Import IBMAR data on flow
-envir.read_IBAMR3d_vtk_data('data/16towers_Re10_len10.vtk')
-print('Domain set to {}.'.format(envir.L))
+print('Reading VTK data. This will take a while...')
+envir.read_IBAMR3d_vtk_data('data/RAW_10x20x2cm_8_5s.vtk')
+print('Domain set to {} mm.'.format(envir.L))
+print('Flow mesh is {}.'.format(envir.flow[0].shape))
+print('-------------------------------------------')
+# Domain should be 80x320x80 mm
+# Model sits (from,to): (2.5,77.5), (85,235), (0.5,20.5)
+# Need: 182 mm downstream from model to capture both zones
 
-envir.add_swarm()
-s = envir.swarms[0]
+# Extend domain downstream so Y is 420mm total length
+envir.extend(y_plus=100)
+print('Domain extended to {} mm'.format(envir.L))
+print('Flow mesh is {}.'.format(envir.flow[0].shape))
+print('-------------------------------------------')
+# NOW:
+# Domain should be 80x420x80 mm
+# Model sits (from,to): (2.5,77.5), (85,235), (0.5,20.5)
+
+# Add swarm right in front of model
+s = envir.add_swarm(swarm_s=1000, init='point', pos=(40,84,1))
 
 # Specify amount of jitter (mean, covariance)
-# Set std as 1 cm = 0.01 m
-shrimp_walk = ([0,0,0], (0.01**2)*np.eye(3))
+# Set sigma**2 as 0.5cm**2/sec =  
+# (sigma**2=2*D, D for brine shrimp given in Kohler, Swank, Haefner, Powell 2010)
+shrimp_walk = ([0,0,0], 50*np.eye(3))
 
 print('Moving swarm...')
 for ii in range(240):
     s.move(0.1, shrimp_walk)
 
-########## This bit is for plotting the cylinders, if so desired ##########
+########## This bit is for cylinders. Change to represent model ##########
 
 def plot_cylinders(ax3d, bounds):
     '''Plot a vertical cylinder on a matplotlib Axes3D object.
