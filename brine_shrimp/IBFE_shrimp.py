412ab2b14d59715ec203a3355142de8030d57c2d
Christopher Strickland <cstric12@utk.edu>
2019-04-18 13:19:19 -0400

    Restructure IBFE_shrimp with functions

--- brine_shrimp/IBFE_shrimp.py
+++ brine_shrimp/IBFE_shrimp.py
@@ -15,9 +15,6 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import Planktos, data_IO
 
-# Whether or not to show the cylinders based on the mesh data
-PLOT_MODEL = True
-
 # Intialize environment
 envir = Planktos.environment()
 
@@ -45,111 +42,51 @@ model_bounds = (2.5,77.5,85,235,0,20.5)
 print('-------------------------------------------')
 
 
-############      Add swarm right in front of model      ############
-s = envir.add_swarm(swarm_s=1000, init='point', pos=(40,84,1))
+############################################################################
+############           DATA GATHERING/SAVING FUNCTIONS          ############
+############################################################################
 
-# Specify amount of jitter (mean, covariance)
-# Set sigma**2 as 0.5cm**2/sec = 50mm**2/sec, sigma~7mm
-# (sigma**2=2*D, D for brine shrimp given in Kohler, Swank, Haefner, Powell 2010)
-shrimp_walk = ([0,0,0], 50*np.eye(3))
+def plot_cell_counts(time_mesh, g_cells_cnts, b_cells_cnts):
+    '''Create plots of cell counts using all time points and save to pdf.'''
+    plot_order = [7,8,5,6,3,4,1,2]
+    g_cells_cnts = np.array(g_cells_cnts)
+    b_cells_cnts = np.array(b_cells_cnts)
 
+    plt.figure(figsize=(4.8, 6.4))
+    for n, plot in enumerate(plot_order):
+        plt.subplot(4,2,plot)
+        plt.plot(time_mesh, g_cells_cnts[:,n])
+        plt.xlabel('time (s)')
+        plt.ylabel('counts')
+        plt.title('Cell number {}'.format(n+1))
+    plt.tight_layout()
+    plt.savefig('green_cell_plots.pdf')
 
-############ Move the swarm according to the prescribed rules above ############
-
-print('Moving swarm...')
-dt = 0.1
-num_of_steps = 240
-for ii in range(num_of_steps):
-    s.move(dt, shrimp_walk)
-
-
-############ Gather data about flow tank observation area ############
-# Observation squares are 2cm**2 until the top
-# Green zone begins right where the model ends, at y=235. Ends at y=275 mm.
-g_bounds = (235, 275)
-# Blue zone begins 14.2-4=10.2 cm later, at y=377 mm. Ends at y=417 mm.
-b_bounds = (377, 417)
-# Each cell is 2cm x 2cm
-cell_size=20
-
-gy_cells = [(g_bounds[0]+20*k, g_bounds[0]+20*k+20) for k in np.arange(8)%2]
-by_cells = [(b_bounds[0]+20*k, b_bounds[0]+20*k+20) for k in np.arange(8)%2]
-z_cells = [(20*k, 20*k+20) for k in np.arange(8)//2]
-
-# Tabulate counts for each cell
-print('Obtaining counts...')
-g_cells_cnts = list()
-b_cells_cnts = list()
-for shrimps in s.pos_history: # each time point in history
-    g_cells_cnts.append([])
-    b_cells_cnts.append([])
-    for gcell, zcell in zip(gy_cells, z_cells): # each green cell
-        g_cells_cnts[-1].append(0)
-        for ii in range(shrimps.shape[0]): # each shrimp
-            if gcell[0] <= shrimps[ii,1] < gcell[1] and\
-               zcell[0] <= shrimps[ii,2] < zcell[1]:
-               g_cells_cnts[-1][-1] += 1
-    for bcell, zcell in zip(by_cells, z_cells): # each blue cell
-        b_cells_cnts[-1].append(0)
-        for ii in range(shrimps.shape[0]): # each shrimp
-            if bcell[0] <= shrimps[ii,1] < bcell[1] and\
-               zcell[0] <= shrimps[ii,2] < zcell[1]:
-               b_cells_cnts[-1][-1] += 1
-
-# append last (current) time point
-g_cells_cnts.append([])
-b_cells_cnts.append([])
-for gcell, zcell in zip(gy_cells, z_cells): # each green cell
-    g_cells_cnts[-1].append(0)
-    for ii in range(s.positions.shape[0]): # each shrimp
-        if gcell[0] <= shrimps[ii,1] < gcell[1] and\
-            zcell[0] <= shrimps[ii,2] < zcell[1]:
-            g_cells_cnts[-1][-1] += 1
-for bcell, zcell in zip(by_cells, z_cells): # each blue cell
-    b_cells_cnts[-1].append(0)
-    for ii in range(s.positions.shape[0]): # each shrimp
-        if bcell[0] <= shrimps[ii,1] < bcell[1] and\
-            zcell[0] <= shrimps[ii,2] < zcell[1]:
-            b_cells_cnts[-1][-1] += 1
+    plt.figure(figsize=(4.8, 6.4))
+    for n, plot in enumerate(plot_order):
+        plt.subplot(4,2,plot)
+        plt.plot(time_mesh, b_cells_cnts[:,n])
+        plt.xlabel('time (s)')
+        plt.ylabel('counts')
+        plt.title('Cell number {}'.format(n+1))
+    plt.tight_layout()
+    plt.savefig('blue_cell_plots.pdf')
 
 
-############ Create plot using all time points ############
-plot_order = [7,8,5,6,3,4,1,2]
-g_cells_cnts = np.array(g_cells_cnts)
-b_cells_cnts = np.array(b_cells_cnts)
-time_mesh = list(envir.time_history)
-time_mesh.append(envir.time)
 
-plt.figure(figsize=(4.8, 6.4))
-for n, plot in enumerate(plot_order):
-    plt.subplot(4,2,plot)
-    plt.plot(time_mesh, g_cells_cnts[:,n])
-    plt.xlabel('time (s)')
-    plt.ylabel('counts')
-    plt.title('Cell number {}'.format(n+1))
-plt.tight_layout()
-plt.savefig('green_cell_plots.pdf')
-
-plt.figure(figsize=(4.8, 6.4))
-for n, plot in enumerate(plot_order):
-    plt.subplot(4,2,plot)
-    plt.plot(time_mesh, b_cells_cnts[:,n])
-    plt.xlabel('time (s)')
-    plt.ylabel('counts')
-    plt.title('Cell number {}'.format(n+1))
-plt.tight_layout()
-plt.savefig('blue_cell_plots.pdf')
+def save_sim_to_excel(time_mesh, g_cells_cnts, b_cells_cnts):
+    '''Save simulation data to an excel spreadsheet using pandas'''
+    # Experimental data was every 0.5 sec
+    df_g = pd.DataFrame(g_cells_cnts[0::5], index=time_mesh[0::5], columns=list(np.arange(1,9)))
+    df_g.to_excel('green_cell_data.xlsx')
+    df_b = pd.DataFrame(b_cells_cnts[0::5], index=time_mesh[0::5], columns=list(np.arange(1,9)))
+    df_b.to_excel('blue_cell_data.xlsx')
 
 
-############ Save data in an excel spreadsheet using pandas ############
-# Experimental data was every 0.5 sec
-df_g = pd.DataFrame(g_cells_cnts[0::5], index=time_mesh[0::5], columns=list(np.arange(1,9)))
-df_g.to_excel('green_cell_data.xlsx')
-df_b = pd.DataFrame(b_cells_cnts[0::5], index=time_mesh[0::5], columns=list(np.arange(1,9)))
-df_b.to_excel('blue_cell_data.xlsx')
-        
-
-############ This bit plots the model as a translucent rectangle ############
+##########################################################################
+############              MOVIE RELATED FUNCTIONS             ############
+##########################################################################
+# These functions to be added to swarm plot list to put structures in the movie
 
 def plot_model_rect(ax3d, bounds):
     '''Plot the model as a translucent rectangular prism
@@ -180,7 +117,6 @@ def plot_model_rect(ax3d, bounds):
     ax3d.plot_surface(xx, y_range[1]*np.ones_like(xx), zz, color="lightgray", alpha=0.2)
 
 
-############ This bit plots the sample areas ############
 
 def plot_sample_areas(ax3d, g_range, b_range, x_range):
     '''Plot the sample areas on the base of the domain
@@ -199,16 +135,101 @@ def plot_sample_areas(ax3d, g_range, b_range, x_range):
     ax3d.plot_surface(xx, yy, np.zeros_like(xx), color="cornflowerblue", alpha=0.2)
 
 
-if PLOT_MODEL:
-    # Add model to plot list
-    envir.plot_structs.append(plot_model_rect)
-    envir.plot_structs_args.append((model_bounds,))
-    # Add sample areas to plot list
-    envir.plot_structs.append(plot_sample_areas)
-    envir.plot_structs_args.append((g_bounds, b_bounds, (0, envir.L[0])))
+############################################################################
+############                MAIN RUNNING FUNCTION               ############
+############################################################################
+
+def main(swarm_size=1000, seed=1, create_movie=False):
+    '''Add swarm and simulate dispersal. Future: run this in loop while altering
+    something to see the effect.'''
+
+    # Add swarm right in front of model
+    s = envir.add_swarm(swarm_s=swarm_size, init='point', pos=(40,84,1), seed=seed)
+
+    # Specify amount of jitter (mean, covariance)
+    # Set sigma**2 as 0.5cm**2/sec = 50mm**2/sec, sigma~7mm
+    # (sigma**2=2*D, D for brine shrimp given in Kohler, Swank, Haefner, Powell 2010)
+    shrimp_walk = ([0,0,0], 50*np.eye(3))
 
 
-############              Plot!               #############
-print('Creating movie...')
-s.plot_all('brine_shrimp_IBFE.mp4', fps=10)
-#s.plot_all()
+    ########## Move the swarm according to the prescribed rules above ##########
+    print('Moving swarm...')
+    dt = 0.1
+    num_of_steps = 240
+    for ii in range(num_of_steps):
+        s.move(dt, shrimp_walk)
+
+
+    ############## Gather data about flow tank observation area ##############
+    # Observation squares are 2cm**2 until the top
+    # Green zone begins right where the model ends, at y=235. Ends at y=275 mm.
+    g_bounds = (235, 275)
+    # Blue zone begins 14.2-4=10.2 cm later, at y=377 mm. Ends at y=417 mm.
+    b_bounds = (377, 417)
+    # Each cell is 2cm x 2cm
+    cell_size=20
+
+    gy_cells = [(g_bounds[0]+cell_size*k, g_bounds[0]+cell_size*k+cell_size) for k in np.arange(8)%2]
+    by_cells = [(b_bounds[0]+cell_size*k, b_bounds[0]+cell_size*k+cell_size) for k in np.arange(8)%2]
+    z_cells = [(cell_size*k, cell_size*k+cell_size) for k in np.arange(8)//2]
+
+    # Tabulate counts for each cell
+    print('Obtaining counts...')
+    g_cells_cnts = list()
+    b_cells_cnts = list()
+    for shrimps in s.pos_history: # each time point in history
+        g_cells_cnts.append([])
+        b_cells_cnts.append([])
+        for gcell, zcell in zip(gy_cells, z_cells): # each green cell
+            g_cells_cnts[-1].append(0)
+            for ii in range(shrimps.shape[0]): # each shrimp
+                if gcell[0] <= shrimps[ii,1] < gcell[1] and\
+                    zcell[0] <= shrimps[ii,2] < zcell[1]:
+                    g_cells_cnts[-1][-1] += 1
+        for bcell, zcell in zip(by_cells, z_cells): # each blue cell
+            b_cells_cnts[-1].append(0)
+            for ii in range(shrimps.shape[0]): # each shrimp
+                if bcell[0] <= shrimps[ii,1] < bcell[1] and\
+                    zcell[0] <= shrimps[ii,2] < zcell[1]:
+                    b_cells_cnts[-1][-1] += 1
+
+    # append last (current) time point
+    g_cells_cnts.append([])
+    b_cells_cnts.append([])
+    for gcell, zcell in zip(gy_cells, z_cells): # each green cell
+        g_cells_cnts[-1].append(0)
+        for ii in range(s.positions.shape[0]): # each shrimp
+            if gcell[0] <= shrimps[ii,1] < gcell[1] and\
+                zcell[0] <= shrimps[ii,2] < zcell[1]:
+                g_cells_cnts[-1][-1] += 1
+    for bcell, zcell in zip(by_cells, z_cells): # each blue cell
+        b_cells_cnts[-1].append(0)
+        for ii in range(s.positions.shape[0]): # each shrimp
+            if bcell[0] <= shrimps[ii,1] < bcell[1] and\
+                zcell[0] <= shrimps[ii,2] < zcell[1]:
+                b_cells_cnts[-1][-1] += 1
+
+    ########## Plot and save the run ##########
+    # Create time mesh for plotting and saving data in excel
+    time_mesh = list(envir.time_history)
+    time_mesh.append(envir.time)
+    # Plot and save the run
+    plot_cell_counts(time_mesh, g_cells_cnts, b_cells_cnts)
+    save_sim_to_excel(time_mesh, g_cells_cnts, b_cells_cnts)
+    
+    ########## Create movie ##########
+    # This takes a long time. Only do it if asked to.
+    if create_movie:
+        # Add model to plot list
+        envir.plot_structs.append(plot_model_rect)
+        envir.plot_structs_args.append((model_bounds,))
+        # Add sample areas to plot list
+        envir.plot_structs.append(plot_sample_areas)
+        envir.plot_structs_args.append((g_bounds, b_bounds, (0, envir.L[0])))
+        # Call plot_all to create movie
+        print('Creating movie...')
+        s.plot_all('brine_shrimp_IBFE.mp4', fps=10)
+
+
+if __name__ == "__main__":
+    main()
