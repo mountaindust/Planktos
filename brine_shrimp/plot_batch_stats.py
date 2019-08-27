3e9476003ac89dd71d88cbab6ba8185906e6cea7
Christopher Strickland <mountaindust@gmail.com>
2019-08-27 17:59:06 -0400

    plot_batch_stats now produces latex table

--- brine_shrimp/plot_batch_stats.py
+++ brine_shrimp/plot_batch_stats.py
@@ -6,14 +6,16 @@ statistics so that distribution shapes can be compared across models.
 from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-dirname = '10min_var2.5_N10000'
+dirname = 'data/results/10min_var2.5_N10000'
 
 folder = Path(dirname)
 
 stat_files = [f for f in folder.iterdir() if f.is_file() and f.name[-4:] == '.npz']
 
 data = {}
+names = []
 
 for f in stat_files:
     fobj = np.load(f) # returns a dictionary-like file obj. query contents w/ .files
@@ -22,5 +24,18 @@ for f in stat_files:
     print(fname+' green/blue crossing fractions: {}, {}'.format(
         fobj['g_cross_frac'], fobj['b_cross_frac']
     ))
+    names.append(fname)
     data[fname] = fobj
 
+# create dataframe
+df = pd.DataFrame(data=[data[fname] for fname in names], index=names, dtype=float)
+df.drop(labels=['g_cross_frac', 'b_cross_frac'], axis=1, inplace=True)
+
+# reorder the columns
+cols = ['g_mean', 'g_median', 'g_mode', 'g_std', 'g_skew', 'g_kurt',
+    'b_mean', 'b_median', 'b_mode', 'b_std', 'b_skew', 'b_kurt']
+df = df[cols]
+
+# save it as latex
+# requires usepackage{booktabs}
+df.to_latex('stat_table.tex')
