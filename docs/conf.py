a4884efc07c22aee52a7026308ed1b323552e8d0
Christopher Strickland <cstric12@utk.edu>
2021-08-23 18:42:18 -0400

    Update release to make readthedocs work

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, W. Christopher Strickland'
 author = 'W. Christopher Strickland'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4'
+release = '0.4.1'
 
 
 # -- General configuration ---------------------------------------------------
