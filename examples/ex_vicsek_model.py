a8799a6b37bdc78d1226a8e82de0c3cb8dac2b36
Christopher Strickland <cstric12@utk.edu>
2022-03-04 10:45:57 -0500

    Running null Vicsek model w/ vertex file

--- examples/ex_vicsek_model.py
+++ examples/ex_vicsek_model.py
@@ -10,7 +10,11 @@ environment without structures the particles have to navigate. Planktos is well
 positioned to aid in research targeted at how flocks and schools persist and 
 react to environmental conditions.
 
-WORK IN PROGRESS!!!!!!!
+The fluid velocity data for this example is large and several instances of fluid 
+velocity fields were tested. Rather than trying to include all this data for 
+download somehow (GBs), it will be available on request. Running this example 
+will result in a null model simulation that includes the mesh data but does not 
+include the fluid velocity data. 
 
 References
 ----------
@@ -25,11 +29,20 @@ import numpy as np
 import planktos
 
 ##### Begin by loading the fluid and mesh. #####
-envir = planktos.environment(x_bndry='periodic', y_bndry='periodic')
-envir.read_IB2d_vtk_data('ib2d_data', 5.0e-5, 1000)
-envir.wrap_flow(periodic_dim=[True, True])
-# domain is 1 x 0.25 m
-envir.read_IB2d_vertex_data('ib2d_data/channel.vertex')
+# with just this line, we get an empty domain with no flow to test things out
+envir = planktos.environment(Lx=1, Ly=0.25, x_bndry='periodic', y_bndry='periodic')
+
+
+##### Load your fluid data and mesh data here! #####
+
+### To load only the IB2d mesh without the flow: ###
+envir.read_IB2d_vertex_data('vicsek_mesh/channel.vertex', res=0.001953125)
+
+### To load both IB2d flow data and mesh. Need to generate flow data first! ###
+# envir.read_IB2d_vtk_data('air_cylinder', 5.0e-5, 2000)
+# envir.wrap_flow(periodic_dim=[True, True])
+# # domain is now 1 x 0.25 m
+# envir.read_IB2d_vertex_data('vicsek_mesh/channel.vertex')
 
 # Set the swarm size
 SWARM_SIZE = 500
@@ -45,11 +58,16 @@ class vicsek(planktos.swarm):
         # set particle speed (abs of velocity) in absence of fluid here
         self.shared_props['v'] = 0.02
         # angle noise will be between [-nu/2, nu/2]
-        self.shared_props['nu'] = 0.1 # used by Vicsek et al.
+        self.shared_props['nu'] = 0.5 # 0.1 used by Vicsek et al.
         # particles will pay attention to other particles within a radius r
         self.shared_props['r'] = 0.1 # constant in Vicsek et al. (r=1)
         # by default, initial velocities are set to the background flow at the 
         #   initial position or zero if there is no flow.
+
+        ### Uniform random angle IC, just to verify things are working ###
+        # rnd_angles = self.rndState.rand(self.positions.shape[0])*2*np.pi
+
+        ### Bias initial vel toward the right to move toward cylinder ###
         # Add a random perturbation with angle between [-nu/2, nu/2]
         rnd_angles = self.get_prop('nu')*\
             self.rndState.rand(self.positions.shape[0]) - self.get_prop('nu')/2
@@ -65,7 +83,7 @@ class vicsek(planktos.swarm):
         #   and averaging their angles
         avg_angles = np.zeros(self.positions.shape[0])
         for n in range(self.positions.shape[0]):
-            pos_diff = self.positions - self.position[n,:]
+            pos_diff = self.positions - self.positions[n,:]
             dist = np.sqrt(pos_diff[:,0]**2 + pos_diff[:,1]**2)
             avg_angles[n] = np.arctan2(
                 np.mean(self.velocities[dist<self.get_prop('r'),1]),
@@ -87,12 +105,20 @@ class vicsek(planktos.swarm):
 
 # create a swarm with initial conditions behind the cylinder
 x_center = 0.1 # +/- 0.025
-y_center = 0.125 # +/- 0.05
+y_center = 0.125 # +/- 0.75
 IC_pos = np.zeros((SWARM_SIZE,2))
 IC_pos[:,0] = (np.random.rand(SWARM_SIZE)-0.5)*0.05 + x_center
-IC_pos[:,1] = (np.random.rand(SWARM_SIZE)-0.5)*0.1 + y_center
+IC_pos[:,1] = (np.random.rand(SWARM_SIZE)-0.5)*0.15 + y_center
 
+# create Vicsek swarm
 swrm = vicsek(swarm_size=SWARM_SIZE, envir=envir, init=IC_pos)
 
+# passive particles for comparsion
+# swrm = planktos.swarm(swarm_size=SWARM_SIZE, envir=envir, init=IC_pos)
+# swrm.shared_props['cov'] *= 0.02**2
 
 # conduct simulation
+for ii in range(72): # 18 seconds w/ quarter second timesteps
+    swrm.move(0.25)
+
+swrm.plot_all(movie_filename='vicsek_null.mp4', fps=4, fluid='vort') # realtime
