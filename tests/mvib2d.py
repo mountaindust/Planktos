c25bc8722027f39639b5b8dd779d961ffb17dfb7
Christopher Strickland <cstric12@utk.edu>
2024-09-20 14:37:25 -0400

    Renames environment.read_IB2d_fluid_data

--- tests/mvib2d.py
+++ tests/mvib2d.py
@@ -8,6 +8,6 @@ sys.path.append('..')
 import planktos
 
 envir = planktos.environment(1,1)
-envir.read_IB2d_vtk_fluid_data('wobbly_beam/viz_IB2d', 5.0e-5, 10)
+envir.read_IB2d_fluid_data('wobbly_beam/viz_IB2d', 5.0e-5, 10)
 envir.read_IB2d_mesh_data('wobbly_beam/viz_IB2d', 5.0e-5, 10)
 envir.plot_flow()
--- tests/visualtest_2d.py
+++ tests/visualtest_2d.py
@@ -9,7 +9,7 @@ sys.path.append('..')
 import planktos
 
 envir = planktos.environment()
-envir.read_IB2d_vtk_fluid_data('data/leaf_data', 1.0e-5, 100, d_start=1)
+envir.read_IB2d_fluid_data('data/leaf_data', 1.0e-5, 100, d_start=1)
 ### Use to test for boundary crossings ###
 envir.read_IB2d_mesh_data('data/leaf_data/leaf.vertex', 1.45)
 envir.add_vertices_to_static_2D_ibmesh()
