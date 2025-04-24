fbe01459139ce8a22b8efd0107f9dfe7931c04f1
Christopher Strickland <cstric12@utk.edu>
2025-04-24 12:44:00 -0400

    Sets up penetration debugging

--- tests/rubberband.py
+++ tests/rubberband.py
@@ -5,25 +5,33 @@ Test planktos on various rubberband simulations
 import numpy as np
 import planktos
 
+rndState = np.random.default_rng(1)
+
 envir = planktos.environment(1,1)
 
 ###### Poroelastic rubberband #######
 envir.read_IB2d_fluid_data('poroelastic_rb/viz_IB2d', 1e-4, 20)
 envir.read_IB2d_mesh_data('poroelastic_rb/viz_IB2d', 1e-4, 20, periodic=True)
-start_positions = np.random.rand(100,2)*0.2 + 0.4
+
+start_positions = rndState.random((100,2))*0.2 + 0.4
 swrm = planktos.swarm(envir=envir, init=start_positions, 
                       ib_condition='sliding', seed=1)
 swrm.shared_props['cov'] *= 0.01
-swrm.plot()
-for ii in range(200):
+# swrm.plot()
+for ii in range(54):
     swrm.move(0.001)
-swrm.plot_all('mvib2d_sliding.mp4')
+swrm.plot()
+# Agent number 89 penetrates in next time step
+
+# for ii in range(150):
+#     swrm.move(0.001)
+#swrm.plot_all('mvib2d_sliding.mp4')
 
 ###### Damped springs rubberband ######
 # envir.read_IB2d_fluid_data('damped_rb/viz_IB2d', 1e-3, 20)
 # envir.read_IB2d_mesh_data('damped_rb/viz_IB2d', 1e-3, 20, periodic=True)
 
-# start_positions = np.random.rand(100,2)*0.2 + 0.4
+# start_positions = rndState.random((100,2))*0.2 + 0.4
 # swrm = planktos.swarm(envir=envir, init=start_positions, 
 #                       ib_condition='sticky', seed=1)
 # swrm.shared_props['cov'] *= 0.005
@@ -36,7 +44,7 @@ swrm.plot_all('mvib2d_sliding.mp4')
 # envir.read_IB2d_fluid_data('moving_rb/viz_IB2d', 1e-4, 5)
 # envir.read_IB2d_mesh_data('moving_rb/viz_IB2d', 1e-4, 5, periodic=True)
 
-# start_positions = np.random.rand(100,2)*0.3 + 0.1
+# start_positions = rndState.random((100,2))*0.3 + 0.1
 # swrm = planktos.swarm(envir=envir, init=start_positions, 
 #                       ib_condition='sticky', seed=1)
 # swrm.shared_props['cov'] *= 0.05
